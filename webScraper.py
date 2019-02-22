dec8594a533edc6ec4f0495f4c118ca3ec094181
Victor Accete <victor.accete@gmail.com>
2019-02-22 00:10:43 -0300

    Organizando em funções

--- webScraper.py
+++ webScraper.py
@@ -1,30 +1,30 @@
 import os
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 
-driver = webdriver.Chrome()
-driver.get("http://dabsistemas.saude.gov.br/sistemas/sisvanV2/relatoriopublico/index")
-print(driver.title)
+def initialize_driver():
+    driver = webdriver.Chrome()
+    driver.get("http://dabsistemas.saude.gov.br/sistemas/sisvanV2/relatoriopublico/index")
+    os.makedirs('relats', exist_ok=True) # Criando a pasta para colocar os relatorios baixados
+    print(driver.title)
+    return driver
 
-#os.makedirs('relats', exist_ok=True) # Criando a pasta para colocar os relatorios baixados
+def choose_report(index, driver):
+    driver.implicitly_wait(6) # para dar tempo de carregar os elementos da página
+    elements = [];
+    # a classe btn-success refere-se aos botões de selecionar relatório
+    elements = driver.find_elements_by_class_name("btn-success");
+    print(elements[index].text)
+    elements[index].click() # clicando no primeiro relatorio (estado nutricional)
 
-driver.implicitly_wait(6) # para dar tempo de carregar os elementos da página
+def year_select(driver):
+    driver.implicitly_wait(6)
+    select = Select(driver.find_element_by_id("nuAno"))
+    select.select_by_visible_text("2008")
 
-# a classe btn-success refere-se aos botões de selecionar relatório
-elements = [];
-elements = driver.find_elements_by_class_name("btn-success");
-print(elements[0])
-print(elements[0].text)
-elements[0].click() # clicando no primeiro relatorio (estado nutricional)
-
-driver.implicitly_wait(6)
-select = Select(driver.find_element_by_id("nuAno"))
-select.select_by_visible_text("2008")
-
-
-#driver.close()
+driver = initialize_driver()
+choose_report(0, driver)
+year_select(driver)
