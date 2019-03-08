18e3b9184cf66a69284172503ee9aed09219df48
Victor Accete <victor.accete@gmail.com>
2019-03-07 22:07:52 -0300

    fazendo loop para baixar todos os anos para ambos os sexos

--- webScraper.py
+++ webScraper.py
@@ -10,30 +10,29 @@ def initialize_driver():
     return driver
 
 def choose_report(index, driver):
-    driver.implicitly_wait(6) # para dar tempo de carregar os elementos da página
+    driver.implicitly_wait(8) # para dar tempo de carregar os elementos da página
     elements = [];
     # a classe btn-success refere-se aos botões de selecionar relatório
     elements = driver.find_elements_by_class_name("btn-success");
-    print(elements[index].text)
     elements[index].click() # clicando no primeiro relatorio (estado nutricional)
 
-def year_select(driver):
-    driver.implicitly_wait(6)
+def year_select(driver, year):
+    driver.implicitly_wait(3)
     select = Select(driver.find_element_by_id("nuAno"))
-    select.select_by_visible_text("2008")
+    select.select_by_visible_text(year)
 
 def month_select(driver):
-    driver.implicitly_wait(2)
+    driver.implicitly_wait(1)
     select = Select(driver.find_element_by_id("nuMes"))
     select.select_by_visible_text("TODOS")
 
 def groupby_select(driver):
-    driver.implicitly_wait(2)
+    driver.implicitly_wait(1)
     select = Select(driver.find_element_by_name("tpFiltro"))
     select.select_by_visible_text("MUNICÍPIO")
 
 def state_and_city_select(driver):
-    driver.implicitly_wait(2)
+    driver.implicitly_wait(1)
     select = Select(driver.find_element_by_id("coUfIbge"))
     select.select_by_visible_text("TODOS")
     driver.implicitly_wait(1)
@@ -41,7 +40,7 @@ def state_and_city_select(driver):
     select.select_by_visible_text("TODOS")
 
 def region_select(driver):
-    driver.implicitly_wait(2)
+    driver.implicitly_wait(1)
     select = Select(driver.find_element_by_name("st_cobertura"))
     select.select_by_visible_text("TODAS")
 
@@ -49,30 +48,43 @@ def age_select(driver):
     driver.implicitly_wait(1)
     select = Select(driver.find_element_by_name("nu_ciclo_vida"))
     select.select_by_visible_text("CRIANÇA")
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(2)
     select = Select(driver.find_element_by_id("nu_idade_inicio"))
     select.select_by_visible_text("0")
     select = Select(driver.find_element_by_id("nu_idade_fim"))
     select.select_by_visible_text("< 2 anos")
 
-def sex_select(driver):
+def sex_select(driver, sex):
     driver.implicitly_wait(1)
     select = Select(driver.find_element_by_name("ds_sexo2"))
-    select.select_by_visible_text("FEMININO")
+    select.select_by_visible_text(sex)
 
 def download_report(driver):
     element = driver.find_element_by_id("Download")
     element.click()
 
-if __name__ == '__main__':
-    driver = initialize_driver()
+def download_single_report(driver, year, sex):
     choose_report(0, driver)
-    year_select(driver)
+    year_select(driver, year)
     month_select(driver)
     groupby_select(driver)
     state_and_city_select(driver)
     region_select(driver)
     age_select(driver)
-    sex_select(driver)
+    sex_select(driver, sex)
     download_report(driver)
 
+def download_all_reports(driver):
+    for i in range(2008, 2019):
+        download_single_report(driver, str(i), 'FEMININO')
+        print("Baixando... ano {} e sexo feminino".format(i))
+        driver.refresh()
+        driver.implicitly_wait(3)
+        print("Baixando... ano {} e sexo masculino".format(i))
+        driver.refresh()
+        driver.implicitly_wait(3)
+
+if __name__ == '__main__':
+    driver = initialize_driver()
+    download_all_reports(driver)
+    driver.quit()
